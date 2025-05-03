2777241590db455fb417ae7fdb95d974c49d3f92
Hanh <hanh425@gmail.com>
2025-05-03 20:17:30 +0800

    chore: add release please

--- lib/store.dart
+++ lib/store.dart
@@ -1,12 +1,4 @@
-import 'dart:async';
-import 'dart:math';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:toastification/toastification.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
 import 'package:mobx/mobx.dart';
-import './main.dart';
 
 part 'store.g.dart';
 
