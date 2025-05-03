0f670239f0d08f363ab058bb840a93039e0c4375
Hanh <hanh425@gmail.com>
2025-05-03 20:21:56 +0800

    feat: home page

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:zcvoter/pages/home.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<ModalRoute<void>> routeObserver = RouteObserver<ModalRoute<void>>();
@@ -9,5 +10,6 @@ final router = GoRouter(
   observers: [routeObserver],
   navigatorKey: navigatorKey,
   routes: [
+    GoRoute(path: '/', builder: (context, state) => const HomePage()),
   ],
 );
